987ec3645da50d14990f134cf854148b0769a0c6
Alexis Estigarribia <estigarribiaalexisuniversidad@gmail.com>
2025-08-22 00:51:26 -0300

    Codigo en un 90% terminado para el challenge agregando multiples cambios

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo');
+    let nombreAmigo = inputAmigo.value;
+
+    if (nombreAmigo == '') {
+        alert('Por favor, escribe un nombre.');
+        return; 
+    }
+    if (amigos.includes(nombreAmigo)) {
+        alert('Este nombre ya fue agregado. Intenta con otro.');
+        inputAmigo.value = ''; 
+        return; 
+    }
+    amigos.push(nombreAmigo);
+    actualizarListaVisual();
+    inputAmigo.value = '';
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('Debes agregar al menos 2 personas para hacer un sorteo.');
+        return;
+    }
+
+    for (let i = amigos.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let k = amigos[i];
+        amigos[i] = amigos[j];
+        amigos[j] = k;
+    }
+
+    let amigoSecretoAsignado = amigos[1];
+    let elementoResultado = document.getElementById('resultado');
+    elementoResultado.innerHTML = `<p>Tu amigo secreto es: <strong>${amigoSecretoAsignado}</strong></p>`;
+
+}
+
+function actualizarListaVisual() {
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let elementoLista = document.createElement('li');
+        elementoLista.textContent = amigos[i];
+        listaHTML.appendChild(elementoLista);
+    }
+}
